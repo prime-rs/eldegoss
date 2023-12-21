b2839798e423ab4dbedaa03c58f0bb8ed98cec11
JLer <jlerxky@live.com>
2023-12-21 17:59:55 +0800

    optim config

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::EldegossId;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(default)]
 #[serde(deny_unknown_fields)]
 pub struct Config {
